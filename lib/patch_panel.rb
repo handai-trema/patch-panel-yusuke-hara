84251680ce00d6fc961a5a1ca36cb84cef6d93e4
Yusuke Hara <y-hara@ist.osaka-u.ac.jp>
2016-10-19 16:10:40 +0900

    remove modified

--- lib/patch_panel.rb
+++ lib/patch_panel.rb
@@ -2,6 +2,7 @@
 class PatchPanel < Trema::Controller
   def start(_args)
     @patch = Hash.new { [] }
+    @mirror = Hash.new{[]}
     logger.info 'PatchPanel started.'
   end
 
@@ -17,6 +18,18 @@ class PatchPanel < Trema::Controller
     @patch[dpid] += [port_a, port_b].sort
   end
 
+  def mirroring(dpid, port_a, port_b)
+    
+    add_mirroring_entries dpid, port_a, port_b
+    @patch[dpid].each do |port_c, port_d|
+      if port_c == port_a then
+        add_mirroring_entries dpid, port_d, port_b
+      elsif port_d == port_a then
+        add_mirroring_entries dpid, port_c, port_b
+      end
+  end
+       
+         
   def delete_patch(dpid, port_a, port_b)
     delete_flow_entries dpid, port_a, port_b
     @patch[dpid] -= [port_a, port_b].sort
@@ -32,6 +45,13 @@ class PatchPanel < Trema::Controller
                       match: Match.new(in_port: port_b),
                       actions: SendOutPort.new(port_a))
   end
+
+  def add_mirroring_entries(dpid,port_in,port_out)
+     send_flow_mod_add(dpid,
+                      match: Match.new(in_port: port_in),
+                      actions: SendOutPort.new(port_out))
+  end
+ 
 
   def delete_flow_entries(dpid, port_a, port_b)
     send_flow_mod_delete(dpid, match: Match.new(in_port: port_a))
